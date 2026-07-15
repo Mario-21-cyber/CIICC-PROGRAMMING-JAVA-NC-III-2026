0d2e0d8d0aae4d9c4e46dfd9a2dea3eba8583a50
John Matthew Malabanan <johnmatthewroyo21@gmail.com@example.com>
2026-07-15 16:11:05 +0800

    Tasks/Tasks1.java

--- Tasks/Task1.java
+++ Tasks/Task1.java
@@ -3,7 +3,7 @@ package Tasks;
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class HelloWorldApp {
+class Task1 {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }
