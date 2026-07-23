17174165a652ca2d21d3b8f51aa857dfc4189cda
John Matthew Malabanan <johnmatthewroyo21@gmail.com@example.com>
2026-07-23 16:03:29 +0800

    Fix byte variable initialization and update output string in Task2.java

--- Tasks/Task2.java
+++ Tasks/Task2.java
@@ -3,14 +3,14 @@ public class Task2 {
 
     public static void main(String[] args) {
 
-        byte b = 3;
+        byte b = 0;
         short s = 1;
         int i = 10;
         float f = 2.0f;
         char c = 'H';
         boolean bool = true;
 
-        String output = "" + c + b + s + i + " w" + b + "rld " + f + " " + bool;
+        String output = "" + c + 3 + s + i + " w" + b + "rld " + f + " " + bool;
 
         System.out.println(output);
     }
