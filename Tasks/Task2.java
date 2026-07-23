ca179fe7c3c02896f64532047f727a490d662f91
John Matthew Malabanan <johnmatthewroyo21@gmail.com@example.com>
2026-07-23 15:52:12 +0800

    Add Task2.java with main method and output formatting

---
+++ Tasks/Task2.java
@@ -0,0 +1,17 @@
+package Tasks;
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        byte b = 3;
+        short s = 1;
+        int i = 10;
+        float f = 2.0f;
+        char c = 'H';
+        boolean bool = true;
+
+        String output = "" + c + b + s + i + " w" + b + "rld " + f + " " + bool;
+
+        System.out.println(output);
+    }
+}
