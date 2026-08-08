2d10152060027428392dbec1782a0b5d49704f97
John Matthew Malabanan <johnmatthewroyo21@gmail.com@example.com>
2026-08-08 15:02:33 +0800

    Implement sumOfSums method to calculate cumulative sums and add main method for testing

--- Tasks/Task8.java
+++ Tasks/Task8.java
@@ -1,5 +1,31 @@
 package Tasks;
 
 public class Task8 {
-    
+
+    public static int sumOfSums(int... numbers) {
+        int total = 0;
+
+        for (int n : numbers) {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+
+            total += sum;
+
+            System.out.println("Parameter " + n +
+                    ": sum = " + sum +
+                    ", cumulative sum = " + total);
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        int total = sumOfSums(4, 5, 10);
+
+        System.out.println("Total sum: " + total);
+    }
 }
